8227852e7391fcff77dc5bce1f218a5dd5b28c80
darksethy <angel.espinoza.zacarias@gmail.com>
2016-12-05 06:21:22 -0500

    Activity Login Login Activity Angel Espinoza

--- app/src/main/java/pe/com/chaskygo/activities/LoginActivity.java
+++ app/src/main/java/pe/com/chaskygo/activities/LoginActivity.java
@@ -10,5 +10,20 @@ import pe.com.chaskygo.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button      signInbutton;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signInbutton = (Button) findViewById(R.id.signInbutton);
+        signInbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+    }
 }
